84b8e4417f4e36bbfa6fa1eb7fcf1b72f949c69b
Tienhg2001 <tienhg201@gmail.com>
2023-03-25 19:15:35 +0700

    commit 19:15 (25Sat)

--- myproject/lesson6/lesson6.ts
+++ myproject/lesson6/lesson6.ts
@@ -1,5 +1,12 @@
 let count2: number = 1;
 
+// count2 = 'name'; // error
+
 count2 = 2;
 
 console.log("count 2: ", count2);
+
+let name2: string[] = ["tien", "phuong", "typescript"];
+name2.push("25");
+
+console.log("check name2: ", name2);
---
+++ myproject/lesson7/lesson7.ts
@@ -0,0 +1,11 @@
+let count = "tien1";
+
+count = "tien";
+
+console.log("check count = ", count);
+
+let test = ["tien", "huong", "phuong", 96];
+
+test.push("25");
+
+console.log("test: ", test);
